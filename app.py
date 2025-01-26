c59202558b795a69f0641577de233b6b890be553
Anderson Mauro <andersonmss@hotmail.com>
2025-01-25 22:04:39 -0300

    Primeiro Projeto

---
+++ app.py
@@ -0,0 +1,15 @@
+# Aula 1 - laço de repetição em listas
+nomes_lista = ["Anderson","Carlos","Jose"]
+
+for nome in nomes_lista:
+    print(nome)
+
+# Aula 2 - laço de repeticao em Dicionario
+
+print("") # linha em branco
+
+nomes_dic = {"Anderson","Carlos","Jose"}
+
+for nome in nomes_dic:
+    print(nome)
+    
